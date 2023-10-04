9cf9e89a3bece966df99473736b59b5798cf4b26
Pablo <pnunfe@gmail.com>
2023-10-04 12:30:53 +0200

    types and useStore added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-
-import { useReducer } from 'react'
-
-// 1. Creamos el estado inicial
-const initialState = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: false
-}
-
-// 2. Creamos el reducer
-function reducer (state, action) {
-  const { type } = action
-  if (type === 'SET_FROM_LANGUAGE') {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage
-    }
-  }
-}
+import { useStore } from './hooks/useStore'
 
 function App () {
-  // 3. Utilizamos el reducer con el estado inicial y el reducer creado.
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const { fromLanguage, setFromLanguage } = useStore()
   return (
     <div>
       <h1>Google Translate</h1>
+      <button onClick={() => {
+        setFromLanguage('es')
+      }}>Español</button>
+        {fromLanguage}
     </div>
   )
 }
---
+++ src/hooks/useStore.ts
@@ -0,0 +1,103 @@
+import { type Action, type State } from '../types'
+import { useReducer } from 'react'
+
+// 1. Creamos el estado inicial
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+// 2. Creamos el reducer
+function reducer (state: State, action: Action) {
+  const { type, payload } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+
+  return state
+}
+
+export function useStore () {
+  // 3. Utilizamos el reducer con el estado inicial y el reducer creado.
+  const [
+    {
+      fromLanguage,
+      toLanguage,
+      fromText,
+      result,
+      loading
+    }, dispatch] = useReducer(reducer, initialState)
+
+  // 4. Creamos las funciones que modifican el estado
+  const interchangeLanguages = () => {
+    dispatch({ type: 'INTERCHANGE_LANGUAGES' })
+  }
+  const setFromLanguage = (payload: string) => {
+    dispatch({ type: 'SET_FROM_LANGUAGE', payload })
+  }
+
+  const setToLanguage = (payload: string) => {
+    dispatch({ type: 'SET_TO_LANGUAGE', payload })
+  }
+
+  const setFromText = (payload: string) => {
+    dispatch({ type: 'SET_FROM_TEXT', payload })
+  }
+
+  const setToResult = (payload: string) => {
+    dispatch({ type: 'SET_RESULT', payload })
+  }
+
+  console.log({ fromLanguage })
+  return {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setToResult
+  }
+}
