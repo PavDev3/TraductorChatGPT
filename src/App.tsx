b82dbbbca3d760b9c4c85a4a7f3f222a1b66ca00
Pablo <pnunfe@gmail.com>
2023-10-06 12:10:08 +0200

    LanguageSelector functional

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import './App.css'
 import { useStore } from './hooks/useStore'
 import { AUTO_LANGUAGE } from './constants'
 import { ArrowsIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
 
 function App () {
-  const { fromLanguage, toLanguage, interchangeLanguages, setFromLanguage } = useStore()
+  const { setToLanguage, interchangeLanguages, setFromLanguage, fromLanguage, toLanguage } = useStore()
   return (
     <Container fluid>
       <Row>
         <h1>Translator with ChatGPT</h1>
         <Col>
-        <h2>From</h2>
-        {fromLanguage}
+          <LanguageSelector
+          type='from'
+          value={fromLanguage}
+          onChange={setFromLanguage} />
         </Col>
         <Col>
         <Button variant='link'disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
@@ -21,8 +24,10 @@ function App () {
           </Button>
         </Col>
         <Col>
-        <h2>To</h2>
-        {toLanguage}
+         <LanguageSelector
+         type='to'
+         value={toLanguage}
+         onChange={setToLanguage} />
         </Col>
       </Row>
     </Container>
--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,9 +1,19 @@
 import { Form } from 'react-bootstrap'
-import { SUPPORTED_LANGUAGES } from '../constants'
+import { SUPPORTED_LANGUAGES, AUTO_LANGUAGE } from '../constants'
+import { type FromLanguage, type Language } from '../types'
 
-export const LanguageSelector = ({ onChange }) => {
+type Props =
+  | { type: 'from', value: FromLanguage, onChange: (language: FromLanguage) => void }
+  | { type: 'to', value: Language, onChange: (language: Language) => void }
+
+export const LanguageSelector = ({ onChange, value, type }: Props) => {
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(event.target.value as Language)
+  }
+
   return (
-        <Form.Select aria-label='Selección de idioma'>
+        <Form.Select aria-label='Selección de idioma' onChange={handleChange} value={value}>
+          {type === 'from' && <option value={AUTO_LANGUAGE}>Detectar idioma</option>}
             {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
                 <option key={key} value={key}>
                 {literal}
